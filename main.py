2bf0cd0fb05fbe0c8d6bb4da5d864b3e55a0f43f
BradonH <Bradon.hopper@students.fhu.edu>
2024-01-25 14:10:26 -0600

    progress on work
    got variables and a thought process down

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
 def sum_of_products(list1, list2):
-    pass
+    for i in list1:
+    #    for y in list2:
+            
 
 if __name__ == '__main__':
-   #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+   num_1 = input("first number")
+   num_2 = input("second number")
+   list1 = [int(x) for x in str(num_1)]
+   list2 = [int(x) for x in str(num_2)]
+   if len(list1) != len(list2)
+   sum_of_products(list1, list2)
+   print(res)
 #hello
