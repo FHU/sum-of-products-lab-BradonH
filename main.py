2982c4281f40fd7f9e64211d74079a4e2736bcea
BradonH <Bradon.hopper@students.fhu.edu>
2024-02-01 11:27:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,22 +9,10 @@ def sum_of_products(list1, list2):
 
 if __name__ == '__main__':
     num_1 = input()
-    if num_1 == False:
-        exit()
     num_2 = input()
-    if num_2 == False:
-        exit()
     list1 = num_1.split()
     list2 = num_2.split()
     list1 = [int(x) for x in (num_1)]
     list2 = [int(x) for x in (num_2)]
     res = 0
-    try:
-        test = len(list1)
-        list2[test-1]
-        test = len(list2)
-        list1[test-1]
-        print(sum_of_products(list1, list2))
-    except:
-        print("Error")
-
+    print(sum_of_products(list1, list2))
