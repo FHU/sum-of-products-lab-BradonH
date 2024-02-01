d2d3f0bf7f813717ca63276810005fa617a5b147
BradonH <Bradon.hopper@students.fhu.edu>
2024-01-31 18:34:08 -0600

    update main.py
    finished lab

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
 def sum_of_products(list1, list2):
-    for i in list1:
-    #    for y in list2:
-            
+    
+    res = 0
+    for i in range(len(list1)):
+        res += list1[i-1]*list2[i-1]
+    return res
 
 if __name__ == '__main__':
-   num_1 = input("first number")
-   num_2 = input("second number")
-   list1 = [int(x) for x in str(num_1)]
-   list2 = [int(x) for x in str(num_2)]
-   if len(list1) != len(list2)
-   sum_of_products(list1, list2)
-   print(res)
-#hello
+    num_1 = input("first number")
+    num_2 = input("second number")
+    list1 = [int(x) for x in str(num_1)]
+    list2 = [int(x) for x in str(num_2)]
+    res = 0
+    if len(list1) != len(list2):
+       exit
+    else:
+       pass
+
+    print(sum_of_products(list1, list2))
+
